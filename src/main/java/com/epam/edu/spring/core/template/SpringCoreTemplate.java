ad7f21419ef799ca51e19cd023fbff2f9259f39f
ranaviira <84rent@gmail.com>
2021-10-31 17:14:49 +0400

    final version

--- src/main/java/com/epam/edu/spring/core/template/SpringCoreTemplate.java
+++ src/main/java/com/epam/edu/spring/core/template/SpringCoreTemplate.java
@@ -1,9 +1,20 @@
 package com.epam.edu.spring.core.template;
 
+import com.epam.edu.spring.core.template.configuration.MainConfiguration;
+
+import com.epam.edu.spring.core.template.service.ItemService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SpringCoreTemplate {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	}
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainConfiguration.class);
+        ApplicationContext applicationContext;
+        applicationContext = context;
+        applicationContext.getBean(ItemService.class);
 
+        context.close();
+    }
 }
--- src/main/java/com/epam/edu/spring/core/template/configuration/RepositoryConfiguration.java
+++ src/main/java/com/epam/edu/spring/core/template/configuration/RepositoryConfiguration.java
@@ -1,4 +1,51 @@
 package com.epam.edu.spring.core.template.configuration;
 
+import com.epam.edu.spring.core.template.repository.ArrayListItemRepository;
+import com.epam.edu.spring.core.template.repository.ItemRepository;
+import com.epam.edu.spring.core.template.repository.LinkedListItemRepository;
+import com.epam.edu.spring.core.template.service.ItemService;
+import com.epam.edu.spring.core.template.service.SimpleItemService;
+import com.epam.edu.spring.core.template.validator.ItemValidator;
+import com.epam.edu.spring.core.template.validator.SimpleItemValidator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
+@Configuration
+@PropertySource("classpath:application.properties")
 public class RepositoryConfiguration {
+
+    @Value("${item.repository.implementation}")
+    String implementation;
+
+    @Bean
+    public ItemRepository itemRepository() {
+        if (implementation.equals("")) {
+            return arrayListItemRepository();
+        }
+        return linkedListItemRepository();
+    }
+
+    @Bean
+    public ArrayListItemRepository arrayListItemRepository() {
+        return new ArrayListItemRepository();
+    }
+
+    @Bean
+    public LinkedListItemRepository linkedListItemRepository() {
+        return new LinkedListItemRepository();
+    }
+
+    @Bean
+    public ItemService itemService() {
+        return new SimpleItemService(itemRepository(), itemValidator());
+    }
+
+    @Bean
+    public ItemValidator itemValidator() {
+        return new SimpleItemValidator();
+    }
+
 }
